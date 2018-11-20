ffbe8f1124c9f3cbcd3f9f81f4fbaa786ea86cbc
EC2 Default User <ec2-user@ip-172-31-33-88.us-east-2.compute.internal>
2018-11-20 18:54:43 +0000

    add tp

---
+++ TD1/phpmyadmin.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "pma" {
+  ami                    = "ami-0ebc281c20e89ba4b"
+  key_name               = "devOpsTp2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.allow_all_egress.id}", "${aws_security_group.http_https.id}", "${aws_security_group.ssh.id}"]
+  subnet_id              = "${aws_subnet.public_subnet.id}"
+  ebs_optimized          = false
+  monitoring             = false
+}
+resource "aws_instance" "test" {
+  ami                    = "ami-0ebc281c20e89ba4b"
+  key_name               = "devOpsTp2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.allow_all_egress.id}", "${aws_security_group.http_https.id}", "${aws_security_group.ssh.id}"]
+  subnet_id              = "${aws_subnet.public_subnet.id}"
+  ebs_optimized          = false
+  monitoring             = false
+}
